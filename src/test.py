d515aa373fa84c47878fdddc4b7a1215ac8a690d
wolfyeva <81672797+wolfyeva@users.noreply.github.com>
2022-05-01 17:02:49 +0800

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,3 +1,5 @@
 import os
 import sys
-os.system("docker-compose -f "+sys.argv[1]+" config")
+import subprocess
+process=os.system("docker-compose -f "+sys.argv[1]+" config -q")
+print(process.returncode)
