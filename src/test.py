576c8d5800fa79c8fc3bfe09bb280874e418625e
wolfyeva <81672797+wolfyeva@users.noreply.github.com>
2022-05-01 17:53:03 +0800

    1

--- src/test.py
+++ src/test.py
@@ -2,5 +2,6 @@ import os
 import sys
 import subprocess
 import pytest
-process=os.system("docker-compose -f "+sys.argv[1]+" config -q")
+with pytest.raises(Exception):
+    process=os.system("docker-compose -f "+sys.argv[1]+" config -q")
 assertFalse(process)
